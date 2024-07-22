e1375c95b2d496f16c1fb489be3752012afcd511
Devvrat077 <divyanshsinghrajput3@gmail.com>
2024-07-22 10:29:39 +0000

    setup

---
+++ basics.js
@@ -0,0 +1,22 @@
+let str1 , str2;
+
+str1 = "devvrat";
+
+str2 = str1;
+
+str2 = "divyansh";
+
+console.log(str2);
+console.log(str1);
+
+let object1 = {
+    email : "divyanshsinghrajput3@gmail.com",
+    ph : 8467873107,
+};
+
+let object2 = object1 ;
+
+object2.ph = 9918859797;
+
+console.log(object2.ph);
+console.log(object1.ph);
