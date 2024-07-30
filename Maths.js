66dcede3608b25e95458db4b86764505d00f538f
Devvrat077 <divyanshsinghrajput3@gmail.com>
2024-07-30 10:22:14 +0000

    maths

---
+++ Maths.js
@@ -0,0 +1,37 @@
+console.log(Math);
+console.log(typeof Math);
+
+console.log(Math.PI);
+console.log(Math.LOG10E);
+console.log(Math.LOG2E);
+console.log(Math.SQRT2);
+
+let X = 2.9999999;
+
+console.log(Math.round(X));
+console.log(Math .ceil(X));
+console.log(Math.floor(X));
+console.log(Math.trunc(X));
+
+let negateX =  444;
+console.log(Math.abs(negateX));
+
+console.log(Math.sqrt(167));
+console.log(Math.sin(-4));
+console.log(Math.pow(2,4));
+console.log(2**4);
+console.log(Math.sign(null));
+console.log(Math.max(1,999,1000,-215,-2000));
+console.log(Math.min(1,999,1000,-2000,-215));
+console.log(Math.random());
+console.log(Math.log(1));
+console.log(Math.E);
+
+console.log((Math.floor((Math.random())*10))+1);
+
+const min =  10;
+const max =  20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
